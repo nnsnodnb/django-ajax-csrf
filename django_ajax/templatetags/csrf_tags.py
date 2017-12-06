ba27303be580a7b34233e2a75743ab06d2918af3
nnsnodnb <ahr63_gej@me.com>
2017-12-07 01:03:19 +0900

    Add mark_safe

--- django_ajax/templatetags/csrf_tags.py
+++ django_ajax/templatetags/csrf_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.utils.safestring import mark_safe
 import os
 
 register = template.Library()
@@ -8,4 +9,5 @@ register = template.Library()
 @register.simple_tag
 def include_ajax_csrf_token():
     url = getattr(settings, 'STATIC_URL') if hasattr(settings, 'STATIC_URL') else getattr(settings, 'MEDIA_URL')
-    return "<script type='text/javascript' src='%s'></script>" % os.path.join(url, 'ajax', 'csrf_token.min.js')
+    return mark_safe('<script type=\'text/javascript\' src=\'%s\'></script>' % os.path.join(url, 'ajax', 'csrf_token.min.js'))
+
