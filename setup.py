47715b6e8472535f62b81e20d6c7619d39201841
nnsnodnb <ahr63_gej@me.com>
2017-12-07 01:03:27 +0900

    Update 0.1.2

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-ajax-csrf',
+    version='0.1.2',
+    description='Django app - bundle csrf token additioner for JavaScript',
+    long_description=open('README.rst', 'r').read(),
+    author='nnsnodnb',
+    author_email='ahr63_gej@me.com',
+    url='https://github.com/nnsnodnb/django-ajax-csrf',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    keywords='django csrf staticfiles',
+    platforms=['any'],
+)
+
